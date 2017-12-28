2185e561f23aebb9e069510c12aae94437f2cbac
Joel Dazo <joel.d.dazo@gmail.com>
2017-12-28 15:59:07 -0500

    developed Python script to process all airline files

--- html_scraping_solution.py
+++ html_scraping_solution.py
@@ -1,3 +1,7 @@
+# OUTPUT: HTML file containing 
+# Make an HTTP request to retrieve airline carrier and airport information
+# and output an HTML file of what the site returns
+
 from bs4 import BeautifulSoup
 import requests
 import json
@@ -5,7 +9,7 @@ import json
 s = requests.Session()
 
 r = s.get('https://www.transtats.bts.gov/Data_Elements.aspx?Data=2')
-soup = BeautifulSoup(r.text)
+soup = BeautifulSoup(r.text, "html5lib")
 viewstate = soup.find(id='__VIEWSTATE')['value']
 eventvalidation = soup.find(id='__EVENTVALIDATION')['value']
 viewstategenerator = soup.find(id='__VIEWSTATEGENERATOR')['value']
@@ -17,11 +21,14 @@ r = s.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
                    ("__VIEWSTATE", viewstate),
                    ("__VIEWSTATEGENERATOR",viewstategenerator),
                    ("__EVENTVALIDATION", eventvalidation),
-                   ("CarrierList", "VX"),
-                   ("AirportList", "BOS"),
+                   ("CarrierList", "AA"),
+                   # ("CarrierList", "VX"),
+                   ("AirportList", "MCO"),
+                   # ("AirportList", "BOS"),
                    ("Submit", "Submit")
                   ))
 
-output_file = "E:/Google Drive/Backpack/Udacity DAND/04 Data Wrangling/virgin_and_logan_airport.html"
+# output_file = "E:/Google Drive/Backpack/Udacity DAND/04 Data Wrangling/virgin_and_logan_airport.html"
+output_file = "E:/Google Drive/Backpack/Udacity DAND/04 Data Wrangling/american_and_orlando.html"
 f = open(output_file,'w')
 f.write(r.text)
