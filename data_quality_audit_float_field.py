6c7abd1819bd9799390cf9ca12686374ff919409
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-08 19:59:13 -0500

    added code to audit floating point fields

--- data_quality_audit_float_field.py
+++ data_quality_audit_float_field.py
@@ -1,14 +1,26 @@
 import csv
 import pprint
-import numpy as np 
+import numpy as np
+import os
 
 fieldname = "wgs84_pos#lat"
 minval = -90
 maxval = 90
 
+OSM_FILE = ""
+SAMPLE_FILE = ""
+COMPUTER_NAME = os.environ['COMPUTERNAME']
+if COMPUTER_NAME == "MELLOYELLO":
+    OSM_FILE = ""
+    SAMPLE_FILE = "D:/Repos/data-wrangling-street-type/sample_chicago.osm"
+elif COMPUTER_NAME == "JDAZO":
+    OSM_FILE = "E:/Repos/data-wrangling-street-type/sample_chicago.osm"
+
+
 def skip_lines(input_file, skip):
     for i in range(0, skip):
         next(input_file)
+
 
 def is_number(s):
     try:
@@ -17,9 +29,9 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def is_array(data):
     isinstance(data, (list, tuple, np.ndarray))
-
 
 
 def audit_float_field(v, counts):
@@ -31,7 +43,7 @@ def audit_float_field(v, counts):
     elif is_array(v):
         counts['arrays'] += 1
     elif not is_number(v):
-        print "Found non number:", ValueError
+        print "Found non number:", v
     else:
         v = float(v)
         if not ((minval < v) and (v < maxval)):
@@ -41,11 +53,13 @@ def audit_float_field(v, counts):
 if __name__ == "__main__":
     input_file = csv.DictReader(open("cities3.csv"))
     skip_lines(input_file, 3)
-    counts = {"nulls": 0, "empties": 0, "arrays":0}
+    counts = {"nulls": 0, "empties": 0, "arrays": 0}
     nrows = 0
     for row in input_file:
+        audit_float_field(row[fieldname], counts)
         nrows += 1
 
     print "num cities:", nrows
     print "nulls:", counts['nulls']
     print "empties:", counts['empties']
+    print "arrays:", counts['arrays']
