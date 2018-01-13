908cc00b3a1714fd9fe8b47ff81d6013ff79085c
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-13 13:10:57 -0500

    script customers above average

--- sql_ps_01.sql
+++ sql_ps_01.sql
@@ -3,10 +3,47 @@ Chinook Database
 https://chinookdatabase.codeplex.com/
 */
 
+-- what year has the largest total sales
 select strftime('%Y',InvoiceDate), sum(Total)
 from invoice
 group by strftime('%Y',InvoiceDate)
 order by sum(Total) desc;
 
 
-"SELECT sql, statement FROM Udacious WHERE queryId = 35;"
+select sum(total)
+from (select count(*) as total
+    from Invoice
+    group by BillingCountry
+    order by total desc
+limit 5);
+
+
+select count
+(*) as total 
+from Invoice
+group by BillingCountry
+order by total desc
+limit 5;
+
+-- Hidden code pieces:
+-- 1 - U
+-- 2 - D
+-- 3 - A
+
+-- Find customers whose total invoice amount is higher than the avarage.
+-- Return the city, state, country and total
+
+select BillingCity, BillingState, BillingCountry, Total
+from Invoice,
+    (select avg(Total) as average
+    from Invoice) as subquery
+where Total > average;
+
+
+-- what is the name, city, state, country, and total
+-- of customers with above average invoice totals?
+select FirstName, LastName, BillingCity, BillingState, BillingCountry, Total
+from Invoice join Customer on Invoice.CustomerId = Customer.CustomerId join
+    (select avg(Total) as average
+    from Invoice) as subquery 
+where Total > average;
