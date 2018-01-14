9ff879c1045d6ae92d2b56fe9639d989527cb548
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-13 21:34:01 -0500

    query customers who bought rock genre

---
+++ sql_ps_03.sql
@@ -0,0 +1,11 @@
+/*
+##  Rock Music Lives on!  After the success of your recent email campaign,  
+##  you're interested in targeting your long standing Rock Music audience!
+##  You'll need to collect a list of emails containing each of your Rock Music listeners.
+
+##  Use your query to return the email, first name, last name, and Genre of all Rock Music listeners!
+##  Return you list ordered alphabetically by email address starting with A.
+##  Can you find a way to deal with duplicate email addresses so no one receives multiple emails?
+*/
+
+
