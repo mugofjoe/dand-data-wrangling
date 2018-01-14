67a4deae546854bc88a68325d3fd30575d053158
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-13 22:56:36 -0500

    query top rock musicians

---
+++ sql_ps_06.sql
@@ -0,0 +1,19 @@
+/*
+##  Now that we know that our customers love rock music, we can decide which musicians to 
+##  invite to play at the concert. 
+
+##  Let's invite the artists who have written the most rock music in our dataset.
+##  Write a query that returns the Artist name and total track count of the top 10 rock bands. 
+*/
+-- 1297
+-- QUERIESRF
+
+select Artist.Name, count(Genre.Name)
+from Genre
+    join Track on Track.GenreId = Genre.GenreId
+    join Album on Album.AlbumId = Track.AlbumId
+    join Artist on Artist.ArtistId = Album.ArtistId
+where Genre.Name = 'Rock'
+group by Artist.Name
+order by count(*) desc
+limit 10;
