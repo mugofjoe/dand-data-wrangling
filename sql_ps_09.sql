08106bcd6e8e513769d6405d440c302430db381a
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-14 19:59:18 -0500

    added MediaType table

--- sql_ps_09.sql
+++ sql_ps_09.sql
@@ -14,15 +14,122 @@ Create the InvoiceLine table.
 
 -- );
 
-
-CREATE TABLE InvoiceLine
+CREATE TABLE [Album]
 (
-    InvoiceLineId INTEGER PRIMARY KEY,
-    UnitPrice NUMERIC(10,2),
-    Quantity INTEGER,
-    FOREIGN KEY (InvoiceId) REFERENCES Invoice (InvoiceId)
+    [AlbumId] INTEGER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [ArtistId] INTEGER NOT NULL,
+    CONSTRAINT [PK_Album] PRIMARY KEY  ([AlbumId]),
+    FOREIGN KEY ([ArtistId]) REFERENCES [Artist] ([ArtistId])
+                ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
+CREATE TABLE [Invoice]
+(
+    [InvoiceId] INTEGER NOT NULL,
+    [CustomerId] INTEGER NOT NULL,
+    [InvoiceDate] DATETIME NOT NULL,
+    [BillingAddress] NVARCHAR(70),
+    [BillingCity] NVARCHAR(40),
+    [BillingState] NVARCHAR(40),
+    [BillingCountry] NVARCHAR(40),
+    [BillingPostalCode] NVARCHAR(10),
+    [Total] NUMERIC(10,2) NOT NULL,
+    CONSTRAINT [PK_Invoice] PRIMARY KEY  ([InvoiceId]),
+    FOREIGN KEY ([CustomerId]) REFERENCES [Customer] ([CustomerId])
+                ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+CREATE UNIQUE INDEX [IPK_Invoice] ON [Invoice]([InvoiceId]);
+CREATE INDEX [IFK_InvoiceCustomerId] ON [Invoice] ([CustomerId]);
+
+CREATE TABLE [Artist]
+(
+    [ArtistId] INTEGER NOT NULL,
+    [Name] NVARCHAR(120),
+    CONSTRAINT [PK_Artist] PRIMARY KEY  ([ArtistId])
+);
+CREATE UNIQUE INDEX [IPK_Artist] ON [Artist]([ArtistId]);
+
+CREATE TABLE [Customer]
+(
+    [CustomerId] INTEGER NOT NULL,
+    [FirstName] NVARCHAR(40) NOT NULL,
+    [LastName] NVARCHAR(20) NOT NULL,
+    [Company] NVARCHAR(80),
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60) NOT NULL,
+    [SupportRepId] INTEGER,
+    CONSTRAINT [PK_Customer] PRIMARY KEY  ([CustomerId]),
+    FOREIGN KEY ([SupportRepId]) REFERENCES [Employee] ([EmployeeId])
+                ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+CREATE UNIQUE INDEX [IPK_Customer] ON [Customer]([CustomerId]);
+CREATE INDEX [IFK_CustomerSupportRepId] ON [Customer] ([SupportRepId]);
+
+
+CREATE TABLE [Employee]
+(
+    [EmployeeId] INTEGER NOT NULL,
+    [LastName] NVARCHAR(20) NOT NULL,
+    [FirstName] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(30),
+    [ReportsTo] INTEGER,
+    [BirthDate] DATETIME,
+    [HireDate] DATETIME,
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60),
+    CONSTRAINT [PK_Employee] PRIMARY KEY  ([EmployeeId]),
+    FOREIGN KEY ([ReportsTo]) REFERENCES [Employee] ([EmployeeId])
+                ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+CREATE UNIQUE INDEX [IPK_Employee] ON [Employee]([EmployeeId]);
+CREATE INDEX [IFK_EmployeeReportsTo] ON [Employee] ([ReportsTo]);
+
+CREATE TABLE [Genre]
+(
+    [GenreId] INTEGER NOT NULL,
+    [Name] NVARCHAR(120),
+    CONSTRAINT [PK_Genre] PRIMARY KEY  ([GenreId])
+);
+CREATE UNIQUE INDEX [IPK_Genre] ON [Genre]([GenreId]);
+
+
+
+CREATE TABLE [InvoiceLine]
+(
+    [InvoiceLineId] INTEGER NOT NULL,
+    [InvoiceId] INTEGER NOT NULL,
+    [TrackId] INTEGER NOT NULL,
+    [UnitPrice] NUMERIC(10,2) NOT NULL,
+    [Quantity] INTEGER NOT NULL,
+    CONSTRAINT [PK_InvoiceLine] PRIMARY KEY  ([InvoiceLineId]),
+    FOREIGN KEY ([InvoiceId]) REFERENCES [Invoice] ([InvoiceId])
                 ON DELETE NO ACTION ON UPDATE NO ACTION,
     FOREIGN KEY ([TrackId]) REFERENCES [Track] ([TrackId])
                 ON DELETE NO ACTION ON UPDATE NO ACTION
-
 );
+CREATE UNIQUE INDEX [IPK_InvoiceLine] ON [InvoiceLine]([InvoiceLineId]);
+CREATE INDEX [IFK_InvoiceLineInvoiceId] ON [InvoiceLine] ([InvoiceId]);
+CREATE INDEX [IFK_InvoiceLineTrackId] ON [InvoiceLine] ([TrackId]);
+
+
+CREATE TABLE [MediaType]
+(
+    [MediaTypeId] INTEGER NOT NULL,
+    [Name] NVARCHAR(120),
+    CONSTRAINT [PK_MediaType] PRIMARY KEY  ([MediaTypeId])
+);
+CREATE UNIQUE INDEX [IPK_MediaType] ON [MediaType]([MediaTypeId]);
