7db7c166f63ed00ac0f08a0c88b873be474f8802
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-17 15:09:01 -0500

    some refactoring

--- case_study_01_mapparser.py
+++ case_study_01_mapparser.py
@@ -12,27 +12,4 @@ the map as value.
 
 Note that your code will be tested with a different data file than the 'example.osm'
 """
-import xml.etree.cElementTree as ET
-import pprint
 
-def count_tags(filename):
-        # YOUR CODE HERE
-
-
-def test():
-
-    tags = count_tags('example.osm')
-    pprint.pprint(tags)
-    assert tags == {'bounds': 1,
-                     'member': 3,
-                     'nd': 4,
-                     'node': 20,
-                     'osm': 1,
-                     'relation': 1,
-                     'tag': 7,
-                     'way': 1}
-
-    
-
-if __name__ == "__main__":
-    test()
