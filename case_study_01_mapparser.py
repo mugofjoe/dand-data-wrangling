982fb69b3d3ff4ef8e0ba6864b114a9d26f125c9
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-17 15:12:22 -0500

    added test function

--- case_study_01_mapparser.py
+++ case_study_01_mapparser.py
@@ -18,3 +18,15 @@ import pprint
 def count_tags(filename):
         # YOUR CODE HERE
 
+def test():
+
+    tags = count_tags('example.osm')
+    pprint.pprint(tags)
+    assert tags == {'bounds': 1,
+                     'member': 3,
+                     'nd': 4,
+                     'node': 20,
+                     'osm': 1,
+                     'relation': 1,
+                     'tag': 7,
+                     'way': 1}
