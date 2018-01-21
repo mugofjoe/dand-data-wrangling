69af1047f4b78cee91e8c1731acc28a6a9528c8b
mugofjoe <joel.d.dazo@gmail.com>
2018-01-20 23:08:19 -0500

    Parse XML generate a collection of values from attribute

--- case_study_03_users.py
+++ case_study_03_users.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import xml.etree.cElementTree as ET
-import pprint
-import re
 """
+TOPIC: Parse XML generate a collection of values from attribute
+
 Your task is to explore the data a bit more.
 The first task is a fun one - find out how many unique users
 have contributed to the map in this particular area!
@@ -11,13 +10,18 @@ have contributed to the map in this particular area!
 The function process_map should return a set of unique user IDs ("uid")
 """
 
+import xml.etree.cElementTree as ET
+import pprint
+import re
+
 def get_user(element):
     return
 
 def process_map(filename):
     users = set()
     for _, element in ET.iterparse(filename):
-        pass
+        if "uid" in element.attrib:
+            users.add(element.attrib["uid"])
 
     return users
 
