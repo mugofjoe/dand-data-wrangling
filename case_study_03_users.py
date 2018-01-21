479725cae841e9f6cf85289a6b5ffda16ae908fa
mugofjoe <joel.d.dazo@gmail.com>
2018-01-20 22:50:04 -0500

    added topic documentation

--- case_study_03_users.py
+++ case_study_03_users.py
@@ -4,3 +4,9 @@ import xml.etree.cElementTree as ET
 import pprint
 import re
 """
+Your task is to explore the data a bit more.
+The first task is a fun one - find out how many unique users
+have contributed to the map in this particular area!
+
+The function process_map should return a set of unique user IDs ("uid")
+"""
