700718d702594db14808e233d0a5fe32f26cd1c2
mugofjoe <joel.d.dazo@gmail.com>
2018-01-22 22:53:06 -0500

    variables and regular expressions

--- case_study_04_improve_street_names.py
+++ case_study_04_improve_street_names.py
@@ -13,3 +13,6 @@ import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
 import pprint
+
+OSMFILE = "example.osm"
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
