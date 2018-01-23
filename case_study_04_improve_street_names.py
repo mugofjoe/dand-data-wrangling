16b0d318f7b024a0b53227d08126e343f064764b
mugofjoe <joel.d.dazo@gmail.com>
2018-01-22 23:13:10 -0500

    added function audit

--- case_study_04_improve_street_names.py
+++ case_study_04_improve_street_names.py
@@ -34,3 +34,15 @@ def audit_street_type(street_types, street_name):
 
 def is_street_name(elem):
     return (elem.attrib['k'] == "addr:street")
+
+def audit(osmfile):
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
+    osm_file.close()
+    return street_types
