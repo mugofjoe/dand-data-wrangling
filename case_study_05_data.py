6c98353f5022bd18a655af54f1081fd625789fe8
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-24 12:44:06 -0500

    added codecs

--- case_study_05_data.py
+++ case_study_05_data.py
@@ -190,7 +190,7 @@ The final return value for a "way" element should look something like:
 """
 import cerberus
 import csv
-import codecs
+import codecs  # standard python endcoder/decoder
 import pprint
 import re
 import schema
@@ -220,6 +220,14 @@ def process_map(file_in, validate):
     """
     Iteratively process each XML element and write to csv(s)
     """  
+
+    # opens the files for writing
+    with codecs.open(NODES_PATH, 'w') as nodes_file, \
+         codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
+         codecs.open(WAYS_PATH, 'w') as ways_file, \
+         codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file, \
+         codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+
 
 if __name__ == '__main__':
     # Note: Validation is ~ 10X slower. For the project consider using a small
