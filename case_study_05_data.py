88fe8bf34b39cf1016296dfbae9637cbb3301924
Joel Dazo <joel.d.dazo@gmail.com>
2018-01-24 12:51:44 -0500

    added extension class for DictWriter

--- case_study_05_data.py
+++ case_study_05_data.py
@@ -213,6 +213,21 @@ WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
 WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
 WAY_NODES_FIELDS = ['id', 'node_id', 'position']
 
+
+
+class UnicodeDictWriter(csv.DictWriter, object):
+    """
+    Extend csv.DictWriter to handle Unicode input
+    """
+    def writerow(self, row):
+        super(UnicodeDictWriter, self).writerow({
+            k: (v.encode('utf-8') if isinstance(v, unicode) else v) for k, v in row.iteritems()
+        })
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
 # ================================================== #
 #               Main Function                        #
 # ================================================== #
@@ -223,10 +238,18 @@ def process_map(file_in, validate):
 
     # opens the files for writing
     with codecs.open(NODES_PATH, 'w') as nodes_file, \
-         codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
-         codecs.open(WAYS_PATH, 'w') as ways_file, \
-         codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file, \
-         codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+        codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
+        codecs.open(WAYS_PATH, 'w') as ways_file, \
+        codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file, \
+        codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+
+        nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
+        node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
+        ways_writer = UnicodeDictWriter(ways_file, WAY_FIELDS)
+        way_nodes_writer = UnicodeDictWriter(way_nodes_file, WAY_NODES_FIELDS)
+        way_tags_writer = UnicodeDictWriter(way_tags_file, WAY_TAGS_FIELDS)     
+
+        nodes_writer    
 
 
 if __name__ == '__main__':
